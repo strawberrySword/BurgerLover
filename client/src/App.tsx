16271c81cde308236094ae3888e5317cc0df5b61
strawberrySword <shaharglame@gmail.com>
2023-05-24 00:32:35 +0300

    feat: user creation and login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,17 @@ import { useState } from 'react'
 function App() {
   const [showMenu, setShowMenu] = useState(false)
 
-  const toggleMenu = () => {setShowMenu(!showMenu)}
+  const toggleMenu = () => { setShowMenu(!showMenu) }
 
   return (
     <div className='w-[100%] h-[100%] flex flex-direction-row justify-evenly'>
       <div className='min-w-[80%] w-[100%] text-center'>Hello World</div>
       {
-        showMenu ? (<div onClick={toggleMenu} className='w-[20%] bg-gray-400 text-center'>Menu</div>) : (<div onClick={toggleMenu} className='w-[5%] bg-gray-400 text-center'>=</div>)
+        showMenu ?
+          (<div onClick={toggleMenu} className='w-[20%] bg-gray-400 text-center'>Menu</div>) :
+          (<div onClick={toggleMenu} className='w-[5%] bg-gray-400 text-center'>=</div>)
       }
-      
+
     </div>
   )
 }
